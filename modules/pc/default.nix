5d845eb4266eef4a6de6d8f71b9179043a8d15ec
makindotcc <9150636+makindotcc@users.noreply.github.com>
2025-01-07 13:21:00 +0100

    try kde

--- modules/pc/default.nix
+++ modules/pc/default.nix
@@ -65,26 +65,35 @@
     LC_TIME = "pl_PL.UTF-8";
   };
 
-  services.xserver = {
-    enable = true;
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
-  };
-  environment.gnome.excludePackages = with pkgs; [
-    totem # "could not initialise opengl support" 😂😂😂 use clapper instead
-  ];
+  # services.xserver = {
+  #   enable = true;
+  #   # displayManager.gdm.enable = true;
+  #   # desktopManager.gnome.enable = true;
+  # };
+  # services.displayManager.sddm.enable = true;
+  # services.displayManager.sddm.wayland.enable = true;
+  # services.desktopManager.plasma6.enable = true;
+
+  # services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # environment.gnome.excludePackages = with pkgs; [
+  #   totem # "could not initialise opengl support" 😂😂😂 use clapper instead
+  # ];
   environment.sessionVariables = {
     # nicer font rendering
     FREETYPE_PROPERTIES = "cff:no-stem-darkening=0 autofitter:no-stem-darkening=0";
     # wayland in (most) electron apps
     NIXOS_OZONE_WL = "1";
   };
-  services.udev.packages = with pkgs; [ gnome-settings-daemon ];
-  qt = {
-    enable = true;
-    platformTheme = "gnome";
-    style = "adwaita-dark";
-  };
+  # services.udev.packages = with pkgs; [ gnome-settings-daemon ];
+  # qt = {
+  #   enable = true;
+  #   platformTheme = "gnome";
+  #   style = "adwaita-dark";
+  # };
 
   fonts = {
     enableDefaultPackages = true;
@@ -107,11 +116,12 @@
     };
   };
 
-  services.xserver.xkb = {
-    layout = "pl";
-    variant = "";
-  };
+  # services.xserver.xkb = {
+  #   layout = "pl";
+  #   variant = "";
+  # };
   console.keyMap = "pl2";
+
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
