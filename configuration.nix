5711df3bca8f1dd4367908c1f3417a95b763f55f
makindotcc <9150636+makindotcc@users.noreply.github.com>
2025-06-22 17:34:58 +0200

    apply system role

--- configuration.nix
+++ configuration.nix
@@ -8,10 +8,12 @@
   pkgs,
   ...
 }:
-
+let
+  stateVersion = "25.05";
+in
 {
   imports = [
-    # Include the results of the hardware scan.
+    ../../roles/system
     ./hardware-configuration.nix
 
     ../../users/makin
@@ -25,7 +27,7 @@
   # Define on which hard drive you want to install Grub.
   # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "makincc";
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
@@ -108,24 +110,8 @@
   # accidentally delete configuration.nix.
   # system.copySystemConfiguration = true;
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = stateVersion;
+  home-manager.sharedModules = [ { home.stateVersion = stateVersion; } ];
 
   boot.loader.grub.devices = [ "/dev/sda" ];
 
