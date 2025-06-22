26a8e9570d5515deadcd775d0be95099c6a4b9c4
makindotcc <9150636+makindotcc@users.noreply.github.com>
2025-06-22 17:31:08 +0200

    add normal user

---
+++ users/makin/default.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  ...
+}:
+{
+  users.users.makin = {
+    isNormalUser = true;
+    description = "makin";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL3BdWxBIA3tyMEF7xiuFQLB85iGHWlROSXNVomxBJ96 user@pc-nixos"
+    ];
+  };
+}
