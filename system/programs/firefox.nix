7df6c927a05a22f3302a2ad53ac7f8fcf9ddbbea
makindotcc <9150636+makindotcc@users.noreply.github.com>
2025-07-12 19:29:07 +0200

    use hm firefox

--- system/programs/firefox.nix
+++ system/programs/firefox.nix
@@ -1,4 +1,24 @@
 { lib, config, ... }:
 {
-  programs.firefox.enable = lib.mkDefault config.isDesktop;
+  options.programs.firefox-hm.enable = lib.mkEnableOption "Firefox with home manager" // {
+    default = config.isDesktop;
+  };
+
+  config = lib.mkIf config.programs.firefox-hm.enable {
+    home-manager.sharedModules = [
+      {
+        programs.firefox = {
+          enable = true;
+          profiles.default = {
+            settings = {
+              "toolkit.legacyUserProfileCustomizations.stylesheets" = false;
+              "browser.uidensity" = 0;
+              "svg.context-properties.content.enabled" = true;
+              "browser.theme.dark-private-windows" = false;
+            };
+          };
+        };
+      }
+    ];
+  };
 }
