c215388f0d3ab8a1a258a7b8c7985d32772d0684
makindotcc <9150636+makindotcc@users.noreply.github.com>
2025-12-29 04:12:18 +0100

    append only makinx backups

--- hosts/pc-nixos/backup.nix
+++ hosts/pc-nixos/backup.nix
@@ -1,3 +1,4 @@
+# todo: move off root user
 {
   self,
   config,
@@ -9,11 +10,12 @@ let
   notifySend =
     args:
     "${pkgs.su}/bin/su -s /bin/sh user -c 'export DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus; ${pkgs.libnotify}/bin/notify-send ${args}'";
+
 in
 {
   environment.systemPackages = [ pkgs.restic ];
 
-  services.restic.backups.backup-fs = {
+  services.restic.backups.fs = {
     repository = "sftp://backupbox/restic/pc";
     passwordFile = config.age.secrets.pc-restic.path;
 
@@ -69,7 +71,7 @@ in
     ];
 
     backupPrepareCommand = ''
-      ${notifySend ''-u normal -i drive-harddisk "Restic Backup" "Rozpoczynam backup do backupbox..."''}
+      ${notifySend ''-u low -i drive-harddisk "Restic Backup" "Rozpoczynam backup do backupbox..."''}
     '';
 
     backupCleanupCommand = ''
@@ -81,5 +83,74 @@ in
     '';
   };
 
-  age.secrets.pc-restic.file = self + /secrets/pc-restic.age;
+  systemd.services.restic-prune-all = {
+    description = "Prune old restic backups";
+
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+
+    script =
+      let
+        pruneOldMakinxBackups = repo: ''
+          echo "Pruning old backups for repository: ${repo}"
+
+          ${pkgs.restic}/bin/restic \
+              -r sftp://backupbox/restic/makinx/${repo} \
+              --password-file "${config.age.secrets.makinx-restic.path}" \
+              forget --prune --keep-within 7d --keep-daily 30 --keep-weekly 12 --keep-monthly 24 \
+              && REPO_STATUS_${lib.toUpper repo}=0 \
+              || REPO_STATUS_${lib.toUpper repo}=$? 
+        '';
+      in
+      ''
+        ${notifySend ''-u low -i user-trash "makinx backup" "Usuwanie starych backupów..."''}
+
+        ${
+          builtins.map pruneOldMakinxBackups [
+            "fs"
+            "pg"
+            "garage"
+          ]
+          |> builtins.concatStringsSep "\n"
+        }
+
+        OVERALL_STATUS=0
+        for repo in fs pg garage; do
+          var_name="REPO_STATUS_$(echo $repo | tr '[:lower:]' '[:upper:]')"
+          eval "status=\$$var_name"
+          if [ "''${status:-0}" -ne 0 ]; then
+            OVERALL_STATUS="$status"
+            break
+          fi
+        done
+
+        if [ $OVERALL_STATUS -eq 0 ]; then
+          ${notifySend ''
+            -u low -i user-trash "makinx backup" \
+            "Usuwanie starych backupów zakończone pomyślnie!"''}
+        else
+          echo "Pruning failed with exit code $OVERALL_STATUS"
+          ${notifySend ''
+            -u critical -i dialog-error "makinx backup" \
+            "Usuwanie starych backupów zakończone błędem (kod: $OVERALL_STATUS)"''}
+          exit $OVERALL_STATUS
+        fi
+      '';
+  };
+
+  systemd.timers.restic-prune-all = {
+    description = "Timer for pruning old restic backups";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "weekly";
+      Persistent = true;
+    };
+  };
+
+  age.secrets = {
+    pc-restic.file = self + /secrets/pc-restic.age;
+    makinx-restic.file = self + /secrets/makinx-restic.age;
+  };
 }
