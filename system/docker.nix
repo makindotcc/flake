956d3b0fdd8fb1598860d0bcc08b3d7bed877f14
makindotcc <9150636+makindotcc@users.noreply.github.com>
2026-02-14 19:00:10 +0100

    wszyscy krzycza z nami wypierdalac z dockerami

--- system/docker.nix
+++ system/docker.nix
@@ -2,7 +2,11 @@
 {
   options.docker.enable = lib.mkEnableOption "Enable Docker support";
   config = lib.mkIf config.docker.enable {
-    virtualisation.docker.enable = true;
+    virtualisation.docker = {
+      enable = true;
+      enableOnBoot = false;
+    };
     environment.persistence.${config.impermanence.dir}.directories = [ "/root/.docker" ];
+
   };
 }
