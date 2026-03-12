f2fdca040cba233814a0a7fe66121ba86256f4ea
makindotcc <9150636+makindotcc@users.noreply.github.com>
2026-03-12 17:09:55 +0100

    flake update

--- system/programs/helium.nix
+++ system/programs/helium.nix
@@ -12,11 +12,11 @@
     let
       helium = pkgs.appimageTools.wrapType2 rec {
         pname = "helium";
-        version = "0.9.4.1";
+        version = "0.10.2.1";
 
         src = pkgs.fetchurl {
           url = "https://github.com/imputnet/helium-linux/releases/download/${version}/${pname}-${version}-x86_64.AppImage";
-          hash = "sha256-N5gdWuxOrIudJx/4nYo4/SKSxakpTFvL4zzByv6Cnug=";
+          hash = "sha256-Kh6UgdleK+L+G4LNiQL2DkQIwS43cyzX+Jo6K0/fX1M=";
         };
 
         extraPkgs =
