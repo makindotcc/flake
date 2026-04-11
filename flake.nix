438f6aee156ff64eafa146e43d8fce09014b81d0
makindotcc <9150636+makindotcc@users.noreply.github.com>
2026-04-11 05:52:40 +0200

    zscrpl

--- flake.nix
+++ flake.nix
@@ -5,27 +5,61 @@
     self.submodules = true;
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    apple-fonts.url = "github:Lyndeno/apple-fonts.nix";
-    dash-to-panel-win11.url = "github:makindotcc/dash-to-panel-win11/ricing";
-    s76-scheduler-plugin.url = "github:makindotcc/s76-scheduler-plugin";
-    apple-emoji-linux.url = "github:samuelngs/apple-emoji-linux/e56448ab6b556c9a3be63ce0fb1903b70fd87b61";
-    urldebloater.url = "github:makindotcc/urldebloater";
-    agenix.url = "github:ryantm/agenix";
-    impermanence.url = "github:nix-community/impermanence";
+    apple-fonts = {
+      url = "github:Lyndeno/apple-fonts.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dash-to-panel-win11 = {
+      url = "github:makindotcc/dash-to-panel-win11/ricing";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    s76-scheduler-plugin = {
+      url = "github:makindotcc/s76-scheduler-plugin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    apple-emoji-linux = {
+      url = "github:samuelngs/apple-emoji-linux/e56448ab6b556c9a3be63ce0fb1903b70fd87b61";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    urldebloater = {
+      url = "github:makindotcc/urldebloater";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    impermanence = {
+      url = "github:nix-community/impermanence";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     vscode-server = {
       url = "github:nix-community/nixos-vscode-server";
       flake = false;
     };
 
-    buzkaaclicker-backend.url = "github:buzkaaclicker/backend-rs";
+    buzkaaclicker-backend = {
+      url = "github:buzkaaclicker/backend-rs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     # buzkaaclicker-backend.url = "path:/home/user/Documents/dev/buzkaaclicker/backend-rs";
-    antibridge.url = "git+ssh://git@github.com/makindotcc/AntiBridge";
+    antibridge = {
+      url = "git+ssh://git@github.com/makindotcc/AntiBridge";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zscrpl = {
+      url = "path:./hosts/makincc/inputs/zscrpl";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     wayfire-src = {
       url = "git+https://github.com/WayfireWM/wayfire?submodules=1";
